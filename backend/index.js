42d074d7c734d2e88cba0787f1e7b33d577ac8ab
dandev947366 <dan.dev947366@gmail.com>
2024-06-17 13:43:02 +0300

    add getblockinfo

--- backend/index.js
+++ backend/index.js
@@ -9,13 +9,14 @@ require("dotenv").config({ path: ".env" });
 app.use(cors());
 app.use(express.json());
 
-const address = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+const eth_address = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 const MORALIS_API_KEY = process.env.MORALIS_API_KEY;
-
+ 
+// Get Eth Price
 app.get("/getethprice", async (req, res) => {
   try {
     const response = await Moralis.EvmApi.token.getTokenPrice({
-      address,
+      address: eth_address,
       chain: '0x1',
     });
 console.log(response)
@@ -25,6 +26,60 @@ console.log(response)
     return res.status(400).json();
   }
 });
+
+// Get Block Info
+app.get("/getblockinfo", async (req, res) => {
+  try {
+    const latestBlock = await Moralis.EvmApi.block.getDateToBlock({
+      date: Date.now(),
+      chain: "0x1",
+    });
+
+    let blockNrOrParentHash = latestBlock.toJSON().block;
+    let previousBlockInfo = [];
+
+    for (let i = 0; i < 5; i++) {
+      const previousBlockNrs = await Moralis.EvmApi.block.getBlock({
+        chain: "0x1",
+        blockNumberOrHash: blockNrOrParentHash,
+      });
+
+      blockNrOrParentHash = previousBlockNrs.toJSON().parent_hash;
+      if (i == 0) {
+        previousBlockInfo.push({
+          transactions: previousBlockNrs.toJSON().transactions.map((i) => {
+            return {
+              transactionHash: i.hash,
+              time: i.block_timestamp,
+              fromAddress: i.from_address,
+              toAddress: i.to_address,
+              value: i.value,
+            };
+          }),
+        });
+      }
+      previousBlockInfo.push({
+        blockNumber: previousBlockNrs.toJSON().number,
+        totalTransactions: previousBlockNrs.toJSON().transaction_count,
+        gasUsed: previousBlockNrs.toJSON().gas_used,
+        miner: previousBlockNrs.toJSON().miner,
+        time: previousBlockNrs.toJSON().timestamp,
+      });
+    }
+
+    const response = {
+      latestBlock: latestBlock.toJSON().block,
+      previousBlockInfo,
+    };
+
+    return res.status(200).json(response);
+  } catch (e) {
+    console.log(`Somthing went wrong ${e}`);
+    return res.status(400).json();
+  }
+});
+
+
 
 app.get("/address", async (req, res) => {
   try {
