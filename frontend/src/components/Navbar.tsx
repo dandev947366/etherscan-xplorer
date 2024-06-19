6d916648d6fe3c9336f8bbc1753a5bde4842bd84
dandev947366 <dan.dev947366@gmail.com>
2024-06-19 18:32:47 +0300

    Update NavBar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,12 +1,42 @@
 "use client";
-import React, { useState, useRef,  } from "react";
+import React, { useState, useRef, useEffect} from "react";
 import Link from "next/link";
 import SearchBar from "@/components/SearchBar";
 import { Search } from "lucide-react";
+import dotenv from "dotenv";
+import axios from 'axios'
+dotenv.config();
+
+const { ETHERSCAN_API_KEY } = process.env;
 
 const Navbar = () => {
-
+  const [userAccount, setUserAccount] = useState("")
+  const [balance, setBalance] = useState("")
+  const [count, setCount] = useState("")
+  const [openModel, setOpenModel] = useState(true)
+  const [price, setPrice] = useState([])
+  const [etherSupply, setEtherSupply] = useState([])
+  const [updatedPriceDate, setUpdatedPriceDate] = useState("")
   
+  const Auth = Buffer.from(
+    process.env.INFURA_API_KEY + ":" + process.env.INFURA_API_KEY_SECRET,
+  ).toString("base64");
+  
+  const chainId = 1; // Ethereum Mainnet
+  
+  (async () => {
+    try {
+      const { data } = await axios.get(
+        `https://gas.api.infura.io/networks/${chainId}/suggestedGasFees`,
+        {
+          headers: { Authorization: `Basic ${Auth}` },
+        },
+      );
+      console.log("Suggested gas fees:", data);
+    } catch (error) {
+      console.log("Server responded with:", error);
+    }
+  })();
   return (
     <div className="navbar bg-gray-100 p-5">
       <div className="container">
